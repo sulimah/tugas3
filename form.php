6c0dcea6633f9267a8c5cbe145f2948094a3c42c
sulimah <sulimeeee@gmail.com>
2022-04-03 19:16:44 +0700

    ini form

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="id">
+    <head>
+        <meta charset="UTF-8"> 
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        
+    </head>
+    <body>
+        <h1>Form tambah data mahasiswa</h1>
+        <form method="post" action="action.php">
+            Nama Mhs: <input type="text" name="nama_mhs"><br/>
+            Nim Mhs: <input type="text" name="nim_mhs"><br/>
+            Alamat Mhs: <input type="text" name="alamat_mhs"><br/>
+            <button type="submit">Kirim</button> 
+        </form>
+    </body>
+</html>
